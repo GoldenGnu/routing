7fce49e0999bb8213bd2d68980d3c93df6c3a87f
GoldenGnu <GoldenGnu@users.noreply.github.com>
2016-05-28 16:13:17 +0200

    Code: Fixed copyright in java files

--- src/main/java/uk/me/candle/eve/routing/engines/Manager.java
+++ src/main/java/uk/me/candle/eve/routing/engines/Manager.java
@@ -1,6 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015-2016, Niklas Kyster Rasmussen, Flaming Candle
+ *
+ * This file is part of Routing
+ *
+ * Routing is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * Routing is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Routing; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 package uk.me.candle.eve.routing.engines;
 
@@ -11,10 +27,7 @@ import java.util.Map;
 import java.util.Random;
 import uk.me.candle.eve.graph.Node;
 
-/**
- *
- * @author Niklas
- */
+
 public class Manager {
 	// Holds our nodes
     private static List<Node> destinationNodes = new ArrayList<Node>();
